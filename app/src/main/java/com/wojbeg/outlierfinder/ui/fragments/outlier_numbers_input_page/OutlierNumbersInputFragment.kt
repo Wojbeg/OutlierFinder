8fa7d9c0b82c858e5ca4bed6c0d92bf4c5721611
Wojciech Begierski <wojtek.begierski@tlen.pl>
2022-04-10 22:00:52 +0200

    add error remover

--- app/src/main/java/com/wojbeg/outlierfinder/ui/fragments/outlier_numbers_input_page/OutlierNumbersInputFragment.kt
+++ app/src/main/java/com/wojbeg/outlierfinder/ui/fragments/outlier_numbers_input_page/OutlierNumbersInputFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -43,6 +44,10 @@ class OutlierNumbersInputFragment : Fragment() {
 
             infoBtn.setOnClickListener {
                 onInfoButtonClick()
+            }
+
+            userNumberInputET.addTextChangedListener {
+                numbersTextInputLayout.error = null
             }
 
         }
